378d778f0843b8b3de068b0899c1a3c05ae8e619
agent19music <seanmotanya@gmail.com>
2023-11-07 13:47:19 +0300

    Update merch components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Animelist from './Animelist.js'
 import Navbar from './Navbar.js';
+import Topmerch from './Topmerch.js';
 
 function App() {
   const [animes, setAnimes] = useState([])
@@ -20,4 +21,23 @@ function App() {
 }
 
 export default App;
- 
+<Switch>
+<Route path="/topmerch">
+    <Topmerch />
+</Route>
+<Route exact path="/animemerch">
+    <Animemerch />
+</Route>
+<Route path="/animes">
+    <Animelist />
+</Route>
+<Route path="/animes/:id">
+    <A />
+</Route>
+<Route exact path="/">
+    <Home />
+</Route>
+<Route path="*">
+    <h1>404 not found</h1>
+</Route>
+</Switch>
