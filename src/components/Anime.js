19e00c3ab55a03d21ae363034ee18ec98a0f1c2d
agent19music <seanmotanya@gmail.com>
2024-05-28 22:55:13 +0300

    Update animelist

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,7 +1,8 @@
-import React,{useState} from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
-export default function Anime({animes, deleteAnime, toggle, isLoading}) {
+
+export default function Anime({ animes, deleteAnime, toggle }) {
   const [imageLoading, setImageLoading] = useState(true);
 
   const handleImageLoad = () => {
@@ -10,66 +11,63 @@ export default function Anime({animes, deleteAnime, toggle, isLoading}) {
 
   const navigate = useNavigate();
 
-function options(anime){
-  Swal.fire({
-    title: "What changes are you making to this anime ?",
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: "Update",
-    denyButtonText: `Delete`
-  }).then((result) => {
-    if (result.isConfirmed) {
-      navigate(`/animelist/${anime.title}`);
-    } else if (result.isDenied) {
-      Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#d33",
-        cancelButtonColor: "gray",
-        confirmButtonText: "Yes, delete it!"
-      }).then((result) => {
-        if (result.isConfirmed) {
-          deleteAnime(anime.id);
-          Swal.fire({
-            title: "Deleted!",
-            text: "The anime has been deleted.",
-            icon: "success"
-          });
-        }
-      });
-      
-    }
-  });
-}
+  function options(anime) {
+    Swal.fire({
+      title: "What changes are you making to this anime?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Update",
+      denyButtonText: `Delete`
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate(`/animelist/${anime.title}`);
+      } else if (result.isDenied) {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#d33",
+          cancelButtonColor: "gray",
+          confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            deleteAnime(anime.id);
+            Swal.fire({
+              title: "Deleted!",
+              text: "The anime has been deleted.",
+              icon: "success"
+            });
+          }
+        });
+      }
+    });
+  }
 
   return (
     <div className='mt-5'>
-     
-         <div className="row">
+      <div className="row">
         {animes.map((anime, index) => (
           <div key={index} className="col-md-3 mt-3" id="picha">
             <div className="card">
-            {imageLoading && (
-                      <div className="spinner-border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                      </div>
-                    )}
-              <img src={anime.poster} className={`card-img-top img-fluid ${imageLoading ? 'd-none' : ''}`} alt="Loading..." onLoad={handleImageLoad} onClick={()=> options(anime)} />
+              {imageLoading && (
+                <div className="spinner-border" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              )}
+              <img src={anime.poster} className={`card-img-top img-fluid ${imageLoading ? 'd-none' : ''}`} alt="Loading..." onLoad={handleImageLoad} onClick={() => options(anime)} />
               <div className={`card-body pd-3 ${toggle}`}>
                 <h5 className="card-title">{anime.title}</h5>
                 <p className="card-text">Rating : {anime.rating}</p>
                 <p className="card-text">Episodes : {anime.episodes}</p>
                 <div className='buttons'>
-                <Link to={`/animemerch/${anime.id}`} className="btn btn-success" >Shop</Link>
+                  <Link to={`/animemerch/${anime.id}`} className="btn btn-success">Shop</Link>
                 </div>
               </div>
             </div>
           </div>
         ))}
       </div>
-     
     </div>
-  )
+  );
 }
--- src/layout/Animelist.js
+++ src/layout/Animelist.js
@@ -1,21 +1,28 @@
-import {React, useContext} from 'react'
-import Anime from '../components/Anime'
-import Footer from './Footer'
-import { SearchContext } from '../context/Searchcontext'
+import { React, useContext } from 'react';
+import { Box, SimpleGrid, Skeleton, SkeletonText } from '@chakra-ui/react';
+import Anime from '../components/Anime';
+import Footer from './Footer';
+import { SearchContext } from '../context/Searchcontext';
 
-export default function Animecollection({ deleteAnime, toggle}) {
-  const {isLoading,animes} = useContext(SearchContext)
-  
+export default function Animelist({ deleteAnime, toggle }) {
+  const { isLoading, animes } = useContext(SearchContext);
+
   return (
     <div className='container'>
-        {isLoading && (
-                      <div className="spinner-border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                      </div>
-                    )}
-     
-      <Anime animes ={animes} key={animes.id} deleteAnime={deleteAnime} toggle={toggle} isLoading={isLoading}/>
-      <Footer/>
+      {isLoading ? (
+        <SimpleGrid columns={[1, null, 3]} spacing="40px">
+          {Array(8).fill().map((_, index) => (
+            <Box key={index} padding="6" boxShadow="lg" bg="white">
+              <Skeleton height="200px" />
+              <SkeletonText mt="4" noOfLines={3} spacing="4" />
+              <Skeleton mt="4" height="40px" />
+            </Box>
+          ))}
+        </SimpleGrid>
+      ) : (
+        <Anime animes={animes} deleteAnime={deleteAnime} toggle={toggle} />
+      )}
+      <Footer />
     </div>
-  )
+  );
 }
