9fbbc48229176af99acc57c4fae8db6519f0683e
agent19music <seanmotanya@gmail.com>
2024-06-01 10:25:42 +0300

    Add chakra UI skeleton for when loading

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,19 +1,17 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import { Box, Skeleton, SkeletonText } from '@chakra-ui/react';
-import { AnimeContext } from '../context/Animecontext';
 
-export default function Anime({ deleteAnime, toggle }) {
+export default function Anime({ animes, deleteAnime, toggle }) {
   const [imageLoading, setImageLoading] = useState(true);
-  const { isLoading, animes } = useContext(AnimeContext);
-  const navigate = useNavigate();
 
   const handleImageLoad = () => {
     setImageLoading(false);
   };
 
-  const options = (anime) => {
+  const navigate = useNavigate();
+
+  function options(anime) {
     Swal.fire({
       title: "What changes are you making to this anime?",
       showDenyButton: true,
@@ -44,24 +42,12 @@ export default function Anime({ deleteAnime, toggle }) {
         });
       }
     });
-  };
+  }
 
   return (
     <div className='mt-5'>
       <div className="row">
-        {isLoading && (
-          Array(8).fill().map((_, index) => (
-            <div key={index} className="col-md-3 mt-3">
-              <Box padding="6" boxShadow="lg" bg="white">
-                <Skeleton height="200px" />
-                <SkeletonText mt="4" noOfLines={3} spacing="4" />
-                <Skeleton mt="4" height="40px" />
-              </Box>
-            </div>
-          ))
-        )}
-
-        {!isLoading && animes.map((anime, index) => (
+        {animes.map((anime, index) => (
           <div key={index} className="col-md-3 mt-3" id="picha">
             <div className="card">
               {imageLoading && (
@@ -69,13 +55,7 @@ export default function Anime({ deleteAnime, toggle }) {
                   <span className="visually-hidden">Loading...</span>
                 </div>
               )}
-              <img 
-                src={anime.poster} 
-                className={`card-img-top img-fluid ${imageLoading ? 'd-none' : ''}`} 
-                alt="Loading..." 
-                onLoad={handleImageLoad} 
-                onClick={() => options(anime)} 
-              />
+              <img src={anime.poster} className={`card-img-top img-fluid ${imageLoading ? 'd-none' : ''}`} alt="Loading..." onLoad={handleImageLoad} onClick={() => options(anime)} />
               <div className={`card-body pd-3 ${toggle}`}>
                 <h5 className="card-title">{anime.title}</h5>
                 <p className="card-text">Rating : {anime.rating}</p>
